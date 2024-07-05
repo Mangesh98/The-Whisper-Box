0ab4266be3feaa44aa167d4906433dc1d0f3995d
Mangesh98 <mangeshraje55555@gmail.com>
2024-07-05 15:14:50 +0530

    Home page created Middleware issue fix

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,32 +1,30 @@
 "use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import React, { useState } from "react";
-import { useToast } from "@/components/ui/use-toast";
-import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { signIn } from "next-auth/react";
 import {
 	Form,
-	FormControl,
 	FormField,
 	FormItem,
 	FormLabel,
 	FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
+import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
 import { SignInSchema } from "@/schemas/signInSchema";
-import { signIn } from "next-auth/react";
+import { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
 
-const page = () => {
-	const [isSubmitting, setIsSubmitting] = useState(false);
-
-	const { toast } = useToast();
+export default function SignInForm() {
 	const router = useRouter();
-
-	// zod implementation
+	const [isSubmitting, setIsSubmitting] = useState(false);
 	const form = useForm<z.infer<typeof SignInSchema>>({
 		resolver: zodResolver(SignInSchema),
 		defaultValues: {
@@ -35,109 +33,107 @@ const page = () => {
 		},
 	});
 
+	const { toast } = useToast();
 	const onSubmit = async (data: z.infer<typeof SignInSchema>) => {
 		setIsSubmitting(true);
-		const result = await signIn("credentials", {
-			identifier: data.identifier,
-			password: data.password,
-			redirect: false,
-		});
-		if (result?.error) {
-			if (result.error === "CredentialsSignin") {
-				toast({
-					title: "Login Failed",
-					description: "Invalid email or password",
-					variant: "destructive",
-				});
-			} else {
-				toast({
-					title: "Login Failed",
-					description: result.error,
-					variant: "destructive",
-				});
-			}
-		}
+		try {
+			const result = await signIn("credentials", {
+				redirect: false,
+				identifier: data.identifier,
+				password: data.password,
+			});
 
-		// if (result?.url) {
-		// 	router.replace("/dashboard");
-		// }
-		setIsSubmitting(false);
+			if (result?.error) {
+				if (result.error === "CredentialsSignin") {
+					toast({
+						title: "Login Failed",
+						description: "Incorrect username or password",
+						variant: "destructive",
+					});
+				} else {
+					toast({
+						title: "Error",
+						description: result.error,
+						variant: "destructive",
+					});
+				}
+			}
+
+			if (result?.url) {
+				toast({
+					title: "Login Successful !",
+					description: "Welcome back to The Whisper Box",
+					variant: "default",
+				});
+				router.replace("/dashboard");
+			}
+		} catch (error) {
+			console.error(error);
+			const axiosError = error as AxiosError<ApiResponse>;
+			let errorMessage = axiosError.response?.data.message;
+			toast({
+				title: "Error",
+				description: errorMessage,
+				variant: "destructive",
+			});
+		} finally {
+			setIsSubmitting(false);
+		}
 	};
 
 	return (
-		<>
-			<div className="flex justify-center items-center max-h-screen bg-gray-100">
-				<div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-					<div className="text-center">
-						<h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-							Join The Whisper Box
-						</h1>
-						<p className="mb-4">Sign In to start your anonymous adventure</p>
-					</div>
-					<Form {...form}>
-						<form onSubmit={form.handleSubmit(onSubmit)}>
-							<div className="space-y-6">
-								<FormField
-									control={form.control}
-									name="identifier"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Email/Username</FormLabel>
-											<FormControl>
-												<Input
-													type="text"
-													placeholder="email/username"
-													{...field}
-												/>
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="password"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Password</FormLabel>
-											<FormControl>
-												<Input
-													type="password"
-													placeholder="password"
-													{...field}
-												/>
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<Button type="submit" disabled={isSubmitting}>
-									{isSubmitting ? (
-										<>
-											<Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please
-											wait
-										</>
-									) : (
-										"Sign In"
-									)}
-								</Button>
-							</div>
-						</form>
-					</Form>
-					<div className="text-center mt-4">
-						<p>
-							Don't have Account?
-							<Link
-								href="/sign-up"
-								className="text-blue-600 hover:text-blue-800"
-							>
-								Sign Up
-							</Link>
-						</p>
-					</div>
+		<div className="flex justify-center items-center min-h-screen bg-gray-800">
+			<div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+				<div className="text-center">
+					<h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+						Welcome Back to The Whisper Box
+					</h1>
+					<p className="mb-4">Sign in to continue your secret conversations</p>
+				</div>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+						<FormField
+							name="identifier"
+							control={form.control}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email/Username</FormLabel>
+									<Input {...field} />
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							name="password"
+							control={form.control}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<Input type="password" {...field} />
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button className="w-full" type="submit" disabled={isSubmitting}>
+							{isSubmitting ? (
+								<>
+									<Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+								</>
+							) : (
+								"Sign In"
+							)}
+						</Button>
+					</form>
+				</Form>
+				<div className="text-center mt-4">
+					<p>
+						Not a member yet?{" "}
+						<Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+							Sign up
+						</Link>
+					</p>
 				</div>
 			</div>
-		</>
+		</div>
 	);
-};
-export default page;
+}
--- src/app/api/auth/[...nextauth]/option.ts
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -1,10 +1,8 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
-import { User } from "@/model/User";
-import { NextAuthOptions, RequestInternal } from "next-auth";
-import bcrypt from "bcryptjs";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { signIn } from "next-auth/react";
 
 export const authOptions: NextAuthOptions = {
 	providers: [
@@ -12,10 +10,10 @@ export const authOptions: NextAuthOptions = {
 			id: "credentials",
 			name: "Credentials",
 			credentials: {
-				email: { label: "email", type: "text" },
+				email: { label: "Email", type: "text" },
 				password: { label: "Password", type: "password" },
 			},
-			async authorize(credentials: any, req: any) {
+			async authorize(credentials: any): Promise<any> {
 				await dbConnect();
 				try {
 					const user = await UserModel.findOne({
@@ -25,10 +23,10 @@ export const authOptions: NextAuthOptions = {
 						],
 					});
 					if (!user) {
-						throw new Error("User not found");
+						throw new Error("No user found with this email");
 					}
 					if (!user.isVerified) {
-						throw new Error("Please verify your account before login");
+						throw new Error("Please verify your account before logging in");
 					}
 					const isPasswordCorrect = await bcrypt.compare(
 						credentials.password,
@@ -36,38 +34,40 @@ export const authOptions: NextAuthOptions = {
 					);
 					if (isPasswordCorrect) {
 						return user;
+					} else {
+						throw new Error("Incorrect password");
 					}
-					throw new Error("Incorrect credentials");
-				} catch (error: any) {
-					throw new Error(error);
+				} catch (err: any) {
+					throw new Error(err);
 				}
 			},
 		}),
 	],
 	callbacks: {
 		async jwt({ token, user }) {
-			token._id = user._id?.toString();
-			token.isVerified = user.isVerified;
-			token.isAcceptingMessages = user.isAcceptingMessages;
-			token.username = user.username;
+			if (user) {
+				token._id = user._id?.toString(); // Convert ObjectId to string
+				token.isVerified = user.isVerified;
+				token.isAcceptingMessages = user.isAcceptingMessages;
+				token.username = user.username;
+			}
 			return token;
 		},
 		async session({ session, token }) {
-            if(token)
-                {
-                    session.user._id = token._id;
-                    session.user.isVerified = token.isVerified;
-                    session.user.isAcceptingMessages = token.isAcceptingMessages;
-                    session.user.username = token.username;
-                }
+			if (token) {
+				session.user._id = token._id;
+				session.user.isVerified = token.isVerified;
+				session.user.isAcceptingMessages = token.isAcceptingMessages;
+				session.user.username = token.username;
+			}
 			return session;
 		},
-	},
-	pages: {
-		signIn: "/sign-in",
 	},
 	session: {
 		strategy: "jwt",
 	},
 	secret: process.env.NEXT_AUTH_SECRET,
+	pages: {
+		signIn: "/sign-in",
+	},
 };
